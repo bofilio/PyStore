24bb01a43d57073de1e7d3428d2f61d73bde8321
bofilio <hemzatalha@gmail.com>
2021-11-24 13:39:09 +0100

    change whitenoise config

--- EMS/settings.py
+++ EMS/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d$r=t!xds-8fu^h4^dh(6wj7e^o+y%3x6%xftahxqpoilbhlq9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'pystorebackend.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'pystorebackend.herokuapp.com']
 
 # Application definition
 
