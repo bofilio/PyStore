08863ade93632309181690504115a3e4ce64d9ee
bofilio <hemzatalha@gmail.com>
2021-12-06 22:35:26 +0100

    last before interview

--- EMS/urls.py
+++ EMS/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from backend.api.index import home
 
 urlpatterns = [
+    path('',home ),
     path('api/',include('backend.urls')),
     path('jet/', include('jet.urls', 'jet')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-    path('', admin.site.urls),
+    path('admin', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn EMS.wsgi --log-file -
+release: python manage.py migrate
